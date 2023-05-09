f6388c7b1e1db2a27babcda067f9a2920558a75a
moviefan322 <philman202@yahoo.com>
2023-05-09 18:26:46 -0400

    adds backend logic for getting and creating tickets

--- server/controllers/ticketController.js
+++ server/controllers/ticketController.js
@@ -7,14 +7,44 @@ const Ticket = require("../models/ticketModel");
 // @route GET/api/tickets
 // @access private
 const getTickets = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "get tickets" });
+  // Get user from token
+  const user = await User.findById(req.user._id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const tickets = await Ticket.find({ user: req.user._id });
+  res.status(200).json(tickets);
 });
 
 // @desc create new ticket
 // @route POST/api/tickets
 // @access private
 const createTicket = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "create ticket" });
+  const { product, description } = req.body;
+
+  if (!product || !description) {
+    res.status(400);
+    throw new Error("Please fill in all fields");
+  }
+
+  const user = await User.findById(req.user._id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const ticket = await Ticket.create({
+    user: req.user._id,
+    product,
+    description,
+    status: "new",
+  });
+
+  res.status(201).json(ticket);
 });
 
 module.exports = { getTickets, createTicket };
