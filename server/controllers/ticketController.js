670cc9670b09961c0d820541b695b888282d529d
moviefan322 <philman202@yahoo.com>
2023-05-09 18:37:12 -0400

    adds update and delete routes for tickets

--- server/controllers/ticketController.js
+++ server/controllers/ticketController.js
@@ -19,6 +19,33 @@ const getTickets = asyncHandler(async (req, res) => {
   res.status(200).json(tickets);
 });
 
+// @desc get user ticket
+// @route GET/api/tickets/:id
+// @access private
+const getTicket = asyncHandler(async (req, res) => {
+  // Get user from token
+  const user = await User.findById(req.user._id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+
+  if (ticket.user.toString() !== req.user._id.toString()) {
+    res.status(401);
+    throw new Error("You are not authorized to view this ticket");
+  }
+
+  res.status(200).json(ticket);
+});
+
 // @desc create new ticket
 // @route POST/api/tickets
 // @access private
@@ -47,4 +74,74 @@ const createTicket = asyncHandler(async (req, res) => {
   res.status(201).json(ticket);
 });
 
-module.exports = { getTickets, createTicket };
+// @desc delete user ticket
+// @route DELETE/api/tickets/:id
+// @access private
+const deleteTicket = asyncHandler(async (req, res) => {
+  // Get user from token
+  const user = await User.findById(req.user._id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+
+  if (ticket.user.toString() !== req.user._id.toString()) {
+    res.status(401);
+    throw new Error("You are not authorized to view this ticket");
+  }
+
+  await Ticket.findByIdAndDelete(req.params.id);
+
+  res.status(200).json({ sucess: true });
+});
+
+// @desc update user ticket
+// @route PUT/api/tickets/:id
+// @access private
+const updateTicket = asyncHandler(async (req, res) => {
+  // Get user from token
+  const user = await User.findById(req.user._id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+
+  if (ticket.user.toString() !== req.user._id.toString()) {
+    res.status(401);
+    throw new Error("You are not authorized to view this ticket");
+  }
+
+  const updatedTicket = await Ticket.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+    }
+  );
+
+  res.status(200).json(updatedTicket);
+});
+
+module.exports = {
+  getTickets,
+  createTicket,
+  getTicket,
+  deleteTicket,
+  updateTicket,
+};
